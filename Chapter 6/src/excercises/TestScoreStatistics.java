3faac328159746332d1f5d059e7f481dbe4b7bad
NoahGiles <billywerber@gmail.com>
2018-10-01 09:52:53 -0500

    Updated Text

--- Chapter 6/src/excercises/TestScoreStatistics.java
+++ Chapter 6/src/excercises/TestScoreStatistics.java
@@ -8,6 +8,15 @@ public class TestScoreStatistics {
 		// TODO Auto-generated method stub
 		int TestScore;
 		Scanner input = new Scanner(System.in);
+		TestScore = input.nextInt();
+		
+		do
+		{
+			if(TestScore < 0 || TestScore > 100 && TestScore != 999)
+			{
+				System.out.println("Error");
+			}
+		}while(TestScore == 999);
 	}
 
 }
